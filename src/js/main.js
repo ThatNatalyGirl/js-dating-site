f2cac4c5548700af176ea983f7e9d6ce9b7f7490
Nataly <ncgarzon88@gmail.com>
2018-05-23 17:00:13 -0400

    inital commit - form validation copy

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+// class formValidator {
+
+// 	constructor(parameters) {
+		let email = document.querySelector('#term');
+
+		document.body.addEventListener('keyup', function () {
+			// check to see if the string includes . and @
+			if (email.value.includes('.') && email.value.includes('@')) {
+				// email.style.border = "5px solid green";
+				emailInput.classList.remove('fail');
+				emailInput.classList.add('pass');
+			} else {
+				// email.style.border = "5px solid red"; 
+				emailInput.classList.remove('pass');
+				emailInput.classList.add('fail');
+			}
+		})
+// 	}
+// }
