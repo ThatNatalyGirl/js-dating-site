6206cababfb16c183b2b08b4db97292c42780bad
Nataly <ncgarzon88@gmail.com>
2018-05-28 16:54:39 -0400

    Updated look and home page validation works

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,56 @@
+console.log('do you even,')
 // class formValidator {
 
 // 	constructor(parameters) {
-		let email = document.querySelector('#term');
+		let emailInput = document.querySelector('#term');
+		let input = document.querySelector('input');
+		let popup = document.querySelector('.pop-up');		
+		let homeButton = document.querySelector('#submit-home');
+		let infoButton = document.querySelector('#info-home');
 
 		document.body.addEventListener('keyup', function () {
 			// check to see if the string includes . and @
-			if (email.value.includes('.') && email.value.includes('@')) {
+			if (emailInput.value.includes('.') && emailInput.value.includes('@')) {
 				// email.style.border = "5px solid green";
 				emailInput.classList.remove('fail');
 				emailInput.classList.add('pass');
+				popup.style.color = 'transparent'
+				console.log('sweet');
 			} else {
 				// email.style.border = "5px solid red"; 
 				emailInput.classList.remove('pass');
 				emailInput.classList.add('fail');
 			}
 		})
+
+		homeButton.addEventListener('click', function(e){
+				
+			if (input.className === "pass"){
+				
+			} else if(input.className === "fail" || input.value === ''){
+				e.preventDefault();
+				console.log('staaaaaap');
+			 	popup.style.color = '#8a1a15';
+			}
+		})
+
+		infoButton.addEventListener('click', function(){
+			//loop through each input field
+			//if any are invalid - prevent default & give warning
+			//else submit the form to a new page
+			if (){
+
+			}
+		})
+
+
+
+
+
+
+
+
+
+
 // 	}
 // }
